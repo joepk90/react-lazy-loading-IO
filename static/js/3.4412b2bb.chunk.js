c61c30a85485562c7245f499e3c916d55db5400e
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2021-06-15 23:22:03 +0100

    Updates

---
+++ static/js/3.4412b2bb.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-lazy-load"]=this["webpackJsonpreact-lazy-load"]||[]).push([[3],{13:function(e,t,c){"use strict";c.r(t);c(1);var a=c(0);t.default=function(e){var t=e.title,c=e.text,s=e.image;return Object(a.jsxs)("div",{className:"card",children:[Object(a.jsx)("img",{src:s}),Object(a.jsx)("h2",{children:t}),Object(a.jsx)("p",{children:c})]})}}}]);
+//# sourceMappingURL=3.4412b2bb.chunk.js.map
