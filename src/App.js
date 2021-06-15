15d334519f062124e2497459a7219f602f1a8ae6
Joseph Park-Kennaby <jparkkennaby@google.com>
2021-06-15 20:06:42 +0100

    add placeholder content

--- src/App.js
+++ src/App.js
@@ -18,6 +18,11 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <main style={{maxWidth: '300px', margin: 'auto'}}>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquam ullamcorper imperdiet. Donec vitae nunc egestas, malesuada arcu eget, blandit est. Proin sit amet justo eros. Donec sed tristique enim. Proin lorem libero, placerat sit amet urna vitae, consectetur lacinia eros. Aliquam nec ornare justo, vitae pharetra est. Quisque ultrices arcu vel dui porttitor, sed maximus dolor aliquet. Sed sollicitudin, sapien et posuere finibus, purus est tristique mauris, vel pellentesque neque erat vel justo.</p>
+      </main>
+
     </div>
   );
 }
